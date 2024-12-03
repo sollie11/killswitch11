8bbfb2d4db5bcdfaa4f803b3d9c7f75796f3a47b
sollie11 <sollie11@gmail.com>
2024-12-03 12:04:48 -0300

    id-maven-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,5 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
-    "maven-publish"
+    id("maven-publish")
 }
